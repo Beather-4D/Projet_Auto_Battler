12596c9d57f990e69ce2641743cb2d476d292ee5
Almikun <triomphalmirade@gmail.com>
2022-10-28 20:40:09 +0200

    Init battler
    Remplissage de battler
    constructeur, getter, setter

---
+++ Auto_Battler/model/Battler.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Battler {
+	private int rank;
+	private int attack;
+	private int healthPoints;
+	
+	Battler(){
+		this.rank = 0;
+		this.attack = 0;
+		this.healthPoints = 0;
+	}
+	
+	Battler(int rank, int attack, int healthPoints){		
+		this.rank = rank;
+		this.attack = attack;
+		this.healthPoints = healthPoints;
+	}
+	
+	public int getRank() {
+		return rank;
+	}
+	
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public int getHealthPoints() {
+		return healthPoints;
+	}
+
+	public void setHealthPoints(int healthPoints) {
+		this.healthPoints = healthPoints;
+	}
+	
+}
