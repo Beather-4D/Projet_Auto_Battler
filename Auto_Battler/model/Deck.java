039fdb2727858a62e44b1cbaa246db1630c9bda6
Baptiste Fosset <baptistefosset@gmail.com>
2022-11-07 11:40:49 +0100

    class deck

--- Auto_Battler/model/Deck.java
+++ Auto_Battler/model/Deck.java
@@ -1,4 +1,16 @@
 package model;
 
 public class Deck {
+    private List<Battlers> deckBattlers;
+
+}
+public void refreshShops()
+{
+
+}
+
+public void getDeckBattlers()
+{
+    //retourne la liste des battlers
+    return this.getDeckBattlers();
 }
