c5ba4532ed0f3ff25efae61213e49ee3fa0c5311
ReySW <78744484+ReySW@users.noreply.github.com>
2022-11-07 11:50:43 +0100

    Update Player.java
    Player bibi

--- Auto_Battler/model/Player.java
+++ Auto_Battler/model/Player.java
@@ -1,4 +1,38 @@
 package model;
 
 public class Player {
+	private int golds;
+	private int healthPoints;
+	private list hand;
+	private Shop;
+	}
+
+Player(){
+	this.golds=0;
+	this.healthPoints=0;
+}
+
+Player(int golds, int healthPoints){		
+	this.gold=gold;
+	this.healthPoints = healthPoints;
+}
+
+public str name{
+	this.name = name;
+}
+
+public int ShopPosition{
+	this.ShopPosition = ShopPosition;
+}
+
+public int dismiss{
+	this.battler = battler;
+}
+
+public int refreshShop() {
+	return Shop;
+}
+
+public int freezeShop() {
+	return Shop;
 }
