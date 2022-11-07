0fae8c765d902df61a825cf7d887df2c785439ec
Almikun <triomphalmirade@gmail.com>
2022-11-07 11:48:11 +0100

    Initialisation Battleground
    Création du squelette de Battlegrounds

--- Auto_Battler/model/Battleground.java
+++ Auto_Battler/model/Battleground.java
@@ -1,4 +1,34 @@
 package model;
 
+import java.util.LinkedList;
+
 public class Battleground {
+    private LinkedList<Battler> player1Battlers;
+    private LinkedList<Battler> player2Battlers;
+
+    Battleground(){
+        player1Battlers = new LinkedList<Battler>();
+        player2Battlers = new LinkedList<Battler>();
+    }
+
+    public void startFight(){
+
+    }
+
+    public void dealDamage(Player player, int damage){
+
+    }
+
+    private int calculateDamageAmount(){
+        return 0;
+    }
+
+    public LinkedList<Battler> getPlayer1Battlers(){
+        return player1Battlers;
+    }
+    
+    public LinkedList<Battler> getPlayer2Battlers(){
+        return player2Battlers;
+    }
+
 }
